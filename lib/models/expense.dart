7bd47f0cc8c9d8eda94596b8ec8138ebb5202ebb
AlbertoJTD <albertojtd@outlook.com>
2025-05-11 12:45:55 -0600

    Use enum data type

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,16 +1,26 @@
 import 'package:uuid/uuid.dart';
 
 Uuid uuid = const Uuid();
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+  services,
+  streaming
+}
 
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
