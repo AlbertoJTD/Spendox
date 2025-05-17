25e0b13ba0a1dcce0d3253ada90c3e49c8783c7e
AlbertoJTD <albertojtd@outlook.com>
2025-05-17 17:18:33 -0600

    Update app theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,46 @@ import 'package:flutter/material.dart';
 
 import 'package:spendox/widgets/expenses/start_screen.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 0, 0, 0),
+  primary: const Color.fromARGB(255, 0, 0, 0),
+  secondary: const Color.fromARGB(255, 255, 255, 255),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 5
+          ),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primary,
+            foregroundColor: kColorScheme.onPrimary,
+          )
+        ),
+
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+          ),
+        )
+      ),
       home: StartScreen(),
     )
   );
